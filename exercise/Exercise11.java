1bdcc98c7561c915e8cf525fc9a17f8bf5ca135c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-10-25 15:45:26 +0900

    2020-10-25-1600

---
+++ exercise/Exercise11.java
@@ -0,0 +1,39 @@
+/**
+ * @title javaPractice / exercise / Exercise11
+ * @content 配列, Random
+ * @author shika
+ * @date 2020-10-25
+ */
+/*
+ * ◆Exercise11
+ * Ramdomクラスをimportし、インスタンスする。
+ * 乱数の結果によって"グー","チョキ","パー"のいずれかを表示する。
+ *
+ */
+
+import java.util.Random;
+
+public class Exercise11 {
+
+    public static void main(String[] args) {
+        String[] finger = new String[] {"グー","チョキ","パー"};
+
+        Random rdm = new Random();
+
+        //↓解答欄ここから
+
+
+    	
+    }//main()
+
+}//class
+
+/*
+チョキ
+
+チョキ
+
+グー
+
+パー
+*/
