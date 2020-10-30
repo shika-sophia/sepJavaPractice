b5a84fdab017c1360ffcfa10b115bd467ecb4329
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-10-30 11:18:51 +0900

    2020-10-30-1100

---
+++ Exercise14Ans.java
@@ -0,0 +1,16 @@
+/**
+ * @title javaPractice / exercise / Exercise14Ans
+ * @content
+ * @author shika
+ * @date 2020-10-30
+ */
+
+package javaPractice.exercise;
+
+public class Exercise14Ans {
+
+    public static void main(String[] args) {
+
+    }//main()
+
+}//class
